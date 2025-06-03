91caed50e2d23262bd8876c53ba83b28823169d0
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-06-03 11:43:28 +0300

    add invoice dto

--- be-impl/src/main/java/com/technokratos/kirillakhmetov/dto/InvoiceDto.java
+++ be-impl/src/main/java/com/technokratos/kirillakhmetov/dto/InvoiceDto.java
@@ -2,6 +2,12 @@ package com.technokratos.kirillakhmetov.dto;
 
 import java.time.LocalDate;
 
-public record InvoiceDto(Long ownerId, Long invoiceId, String number, LocalDate date, Double sum, Integer count,
-                         Integer countTov) {
+public record InvoiceDto(
+        Long ownerId,
+        Long invoiceId,
+        String number,
+        LocalDate date,
+        Double sum,
+        Integer count,
+        Integer countTov) {
 }
