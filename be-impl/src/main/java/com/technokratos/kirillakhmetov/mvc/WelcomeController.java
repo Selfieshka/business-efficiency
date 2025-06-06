617589856b7738308997c59445d3215dbba51efa
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-06-06 13:05:47 +0300

    refactor: change location

--- be-impl/src/main/java/com/technokratos/kirillakhmetov/mvc/WelcomeController.java
+++ be-impl/src/main/java/com/technokratos/kirillakhmetov/mvc/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.technokratos.kirillakhmetov.controller;
+package com.technokratos.kirillakhmetov.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
