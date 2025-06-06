049d7328947ff6be37e0ead344d8b64a3a1c140e
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-06-06 13:32:49 +0300

    refactor: metrics api

--- be-api/src/main/java/com/technokratos/kirillakhmetov/api/MetricsApi.java
+++ be-api/src/main/java/com/technokratos/kirillakhmetov/api/MetricsApi.java
@@ -1,12 +1,15 @@
 package com.technokratos.kirillakhmetov.api;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Map;
 
-@RequestMapping("/api/metrics")
+@RequestMapping("/api/v1/metrics")
 public interface MetricsApi {
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     Map<String, Map<String, Double>> getMethodMetrics();
 }
