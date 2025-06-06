bac8ea70ae0c0d191b0875b7d59f67fcd9c6cdcb
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-06-06 13:05:23 +0300

    feat: data about the owner is obtained from the database

---
+++ be-impl/src/main/java/com/technokratos/kirillakhmetov/mvc/MainController.java
@@ -0,0 +1,21 @@
+package com.technokratos.kirillakhmetov.mvc;
+
+import com.technokratos.kirillakhmetov.service.OwnerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+@RequiredArgsConstructor
+public class MainController {
+    private final OwnerService ownerService;
+
+    @GetMapping
+    public String mainPage(Model model) {
+        model.addAttribute("owner", ownerService.getProfileInfo("kirill@gmail.com"));
+        return "main";
+    }
+}
